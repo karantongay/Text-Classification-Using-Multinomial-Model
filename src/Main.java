086a206dcb80fd2651bf3e9ba33851d573b482b2
karantongay <karantongay@Karans-Air.hitronhub.home>
2018-09-30 16:33:46 -0700

    Final Working Code

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,47 @@
 import com.mongodb.*;
-
 import java.io.*;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-
-import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.*;
 import org.bson.Document;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.util.Arrays;
-
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.AggregateIterable;
 import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Accumulators;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.result.DeleteResult;
 import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     private int totalTermsInClass[] = new int[2];
-    private BigDecimal condProb = new BigDecimal(BigInteger.ONE, MathContext.DECIMAL64);
-    private BigDecimal classOneProb, classTwoProb;
+    private MongoCollection table;
+    private MongoCollection tokenCollectionClassOne, tokenCollectionClassTwo, testDB, testTokenDBClassOne, testTokenDBClassTwo;
+    private double class1Prior, class2Prior;
 
-    private int addTokens(int klas, MongoCollection tokenCollection, MongoCollection vocabularyCollection){
+    private Main(){
+        MongoDatabase db;
+        db = new MongoClient( "localhost" , 27017 ).getDatabase("Vocabulary");
+        table = db.getCollection("vocabulary");
+        tokenCollectionClassOne = db.getCollection("tokenCollectionClassOne");
+        tokenCollectionClassTwo = db.getCollection("tokenCollectionClassTwo");
+        testDB = db.getCollection("testDB");
+        testTokenDBClassOne = db.getCollection("testTokenDBClassOne");
+        testTokenDBClassTwo = db.getCollection("testTokenDBClassTwo");
+    }
+
+    private void addTokens(int klas, MongoCollection tokenCollection, MongoCollection vocabularyCollection, boolean train){
         tokenCollection.drop();
         ArrayList a = new ArrayList();
         MongoCursor<Document> cursor = vocabularyCollection.find(eq("class", klas)).iterator();
         try {
             while (cursor.hasNext()) {
-                int counter = 0;
                 for(String s : cursor.next().get("document").toString().split(" ")){
                     a.add(s);
-                    counter++;
-                    Document d = new Document();
-                    d.put("token", s);
-                    d.put("count", counter);
-                    d.put("class", klas);
-                    tokenCollection.insertOne(d);
-//                    tokenCollection.updateOne(eq("token", s), new Document("$set", d), new UpdateOptions().upsert(true));
+                    if(tokenCollection.count(eq("token", s))==0){
+                        Document d = new Document();
+                        d.put("_id", s);
+                        d.put("token", s);
+                        d.put("count", 1);
+                        d.put("class", klas);
+                        tokenCollection.insertOne(d);
+                    } else {
+                        tokenCollection.updateOne(and(eq("_id", s), eq("token", s)), inc("count", 1));
+                    }
                 }
             }
         }
@@ -56,22 +51,22 @@ public class Main {
         finally {
             cursor.close();
         }
-        System.out.println(a.size());
-        return a.size();
+        if(train){
+            totalTermsInClass[klas] = a.size();
+        }
     }
 
-    private BigDecimal computeConditionalProbability(int klas, MongoCollection tokenCollection){
-        long termCount, totalTermsInAllClasses;
-        MongoCursor<Document> tokenCursor = tokenCollection.find(eq("class", klas)).iterator();
-            BigDecimal b;
+    private double computeConditionalProbability(int klas, MongoCollection tokenCollection){
+        double termCount;
+        double condProb = 1.0;
+        double totalTermsInAllClasses;
+        MongoCursor<Document> tokenCursor = tokenCollection.find().iterator();
             try {
                 while (tokenCursor.hasNext()) {
-                    for (String s : tokenCursor.next().get("token").toString().split("")) {
-                        termCount = Integer.parseInt(tokenCursor.next().get("count").toString());
-                        totalTermsInAllClasses = tokenCollection.count();
-                        b = new BigDecimal((termCount + 1.0) / (totalTermsInClass[klas] + totalTermsInAllClasses), MathContext.DECIMAL64);
-                        condProb = condProb.multiply(b, MathContext.DECIMAL64);
-                    }
+                    Document d = tokenCursor.next();
+                    totalTermsInAllClasses = tokenCollectionClassOne.count() + tokenCollectionClassTwo.count();
+                    termCount = Double.parseDouble(d.get("count").toString());
+                    condProb += (double)(termCount + 1.0) / ((double)totalTermsInClass[klas] + (double)totalTermsInAllClasses);
                 }
             } catch (Exception e) {
                 System.out.println(e);
@@ -81,63 +76,69 @@ public class Main {
             return condProb;
     }
 
-    private void trainMultinomial() throws FileNotFoundException{
-        MongoClient mongo = new MongoClient( "localhost" , 27017 );
-        MongoDatabase db = mongo.getDatabase("Vocabulary");
+    private String trainMultinomial() throws FileNotFoundException{
+        pullData("/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/traindata.txt", "/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/trainlabels.txt", table);
+        double classOneProb = 1.0, classTwoProb = 1.0;
+        double classOneCount = table.count(eq("class", 0));
+        double classTwoCount = table.count(eq("class", 1));
+        class1Prior = (classOneCount / (classOneCount + classTwoCount));
+        class2Prior = (classTwoCount / (classOneCount + classTwoCount));
+        addTokens(0, tokenCollectionClassOne, table, true);
+        addTokens(1, tokenCollectionClassTwo, table, true);
+        classOneProb = computeConditionalProbability(0, tokenCollectionClassOne);
+        classTwoProb = computeConditionalProbability(1, tokenCollectionClassTwo);
+        String str = "Training Metrics: \n" +
+                "Class 0: " + (classOneProb * class1Prior)*100 + "%\n" +
+                "Class 1: " + (classTwoProb * class1Prior)*100 + "%\n";
 
-        BufferedReader br1 = new BufferedReader(new FileReader(new File("/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/traindata.txt")));
-        BufferedReader br2 = new BufferedReader(new FileReader(new File("/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/trainlabels.txt")));
+        return str;
+    }
 
+    private String testMultinomial() throws IOException {
+        pullData("/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/testdata.txt", "/Users/karantongay/UVic/CSC 578 D Data Mining/Assignment 1/testlabels.txt", testDB);
+        addTokens(0, testTokenDBClassOne, testDB, false);
+        addTokens(1, testTokenDBClassTwo, testDB, false);
+        MongoCursor<Document> tokenCursor = testTokenDBClassOne.find().iterator();
+        double classOneCondProb = computeConditionalProbability(0, testTokenDBClassOne);
+        double classTwoCondProb = computeConditionalProbability(1, testTokenDBClassTwo);
+        String str = "Testing Metrics: \n" +
+                "Class 0: " + (classOneCondProb * class1Prior) * 100 + "%\n" +
+                "Class 1: " + (classTwoCondProb * class2Prior) * 100 + "%\n";
+
+        return str;
+    }
+
+    private void pullData(String path1, String path2, MongoCollection DB) throws FileNotFoundException{
+        DB.drop();
+        BufferedReader br1 = new BufferedReader(new FileReader(new File(path1)));
+        BufferedReader br2 = new BufferedReader(new FileReader(new File(path2)));
         String s1,s2;
-        MongoCollection table = db.getCollection("vocabulary");
-        MongoCollection tokenCollectionClassOne = db.getCollection("tokenCollectionClassOne");
-        MongoCollection tokenCollectionClassTwo = db.getCollection("tokenCollectionClassTwo");
-        table.drop();
-
         try {
             while ((s1 = br1.readLine())!= null) {
                 if((s2 = br2.readLine())!= null){
                     Document d = new Document();
                     d.put("document", s1);
                     d.put("class", Integer.parseInt(s2));
-                    table.insertOne(d);
+                    DB.insertOne(d);
                 }
             }
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
 
-        Block<Document> printBlock = new Block<Document>() {
-            @Override
-            public void apply(final Document document) {
-                System.out.println(document.toJson());
-            }
-        };
-
-        double classOneCount = table.count(eq("class", 0));
-        double classTwoCount = table.count(eq("class", 1));
-
-        double class1Prior = (classOneCount / (classOneCount + classTwoCount));
-        double class2Prior = (classTwoCount / (classOneCount + classTwoCount));
-
-//        condProb = (termCount + 1) / (totalDistinctTerms + totalTermsInAllClasses)
-
-        totalTermsInClass[0] = addTokens(0, tokenCollectionClassOne, table);
-        totalTermsInClass[1] = addTokens(1, tokenCollectionClassTwo, table);
-
-        classOneProb = computeConditionalProbability(0, tokenCollectionClassOne);
-        classTwoProb = computeConditionalProbability(1, tokenCollectionClassTwo);
-
-        BigDecimal ch1 = classOneProb.multiply(new BigDecimal(class1Prior, MathContext.DECIMAL64));
-
-        System.out.println(ch1.round(MathContext.DECIMAL32));
-        System.out.println(classTwoProb.multiply(new BigDecimal(class2Prior, MathContext.DECIMAL64)));
+    public void writeResults(String s) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
+        writer.write(s);
+        writer.close();
     }
 
     public static void main(String[] args) throws Exception {
-
         Main obj = new Main();
-        obj.trainMultinomial();
+        String results;
+        results = obj.trainMultinomial();
+        results += obj.testMultinomial();
+        obj.writeResults(results);
 
     }
 }
